e09973bfcd61c3128d04b403a93a1ff8c4558bb3
Miyuki <shibamiyuki07@yahoo.com>
2023-07-20 09:56:41 +0300

    Update Verification_Total.py

--- Sprint1/Verification_Total.py
+++ Sprint1/Verification_Total.py
@@ -1,2 +1,17 @@
+import pymongo
+import sys
+from datetime import datetime
+
+#def getTotal_usage_jour_global_daily_usage(client,day):
+
+
+def getTotal_usage_jout_daily_usage(client,day):
+  pipeline = []
+
+  
+
 if __name__ == "__main__":
-    print("test")
+    client = pymongo.MongoClient("mongodb://oma_dwh:Dwh4%40OrnZ@192.168.61.199:27017/?authMechanism=DEFAULT")
+    date = sys.argv[1]
+    datetime = datetime.strptime(date,'%Y-%m-%d').date()
+    
