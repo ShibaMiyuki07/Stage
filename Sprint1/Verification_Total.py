974c62b5cb8aa828ad0cdc67d155edef316498ab
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-07-20 10:03:48 +0300

    Update Verification_Total.py

--- Sprint1/Verification_Total.py
+++ Sprint1/Verification_Total.py
@@ -5,13 +5,118 @@ from datetime import datetime
 #def getTotal_usage_jour_global_daily_usage(client,day):
 
 
-def getTotal_usage_jout_daily_usage(client,day):
-  pipeline = []
-
+'''def getTotal_usage_jour_daily_usage(client,day):
+    pipeline = [
+    {
+        '$match': {
+            'day': day
+        }
+    }, {
+        '$unwind': {
+            'path': '$usage', 
+            'includeArrayIndex': 'u', 
+            'preserveNullAndEmptyArrays': False
+        }
+    }, {
+        '$unwind': {
+            'path': '$usage.usage_op', 
+            'includeArrayIndex': 'b_s', 
+            'preserveNullAndEmptyArrays': False
+        }
+    }, {
+        '$group': {
+            '_id': '$day', 
+            'sms_i_cnt': {
+                '$sum': '$usage.sms_i_cnt'
+            }, 
+            'voice_i_cnt': {
+                '$sum': '$usage.voice_i_cnt'
+            }, 
+            'voice_i_vol': {
+                '$sum': '$usage.voice_i_vol'
+            }, 
+            'voice_i_amnt': {
+                '$sum': '$usage.voice_i_amnt'
+            }, 
+            'voice_o_cnt': {
+                '$sum': '$usage.usage_op.voice_o_cnt'
+            }, 
+            'voice_o_main_vol': {
+                '$sum': '$usage.usage_op.voice_o_main_vol'
+            }, 
+            'voice_o_amnt': {
+                '$sum': '$usage.usage_op.voice_o_amnt'
+            }, 
+            'voice_o_bndle_vol': {
+                '$sum': '$usage.usage_op.voice_o_bndl_vol'
+            }, 
+            'sms_o_main_cnt': {
+                '$sum': '$usage.usage_op.sms_o_main_cnt'
+            }, 
+            'sms_o_bndl_cnt': {
+                '$sum': '$usage.usage_op.sms_o_bndl_cnt'
+            }, 
+            'sms_o_amnt': {
+                '$sum': '$usage.usage_op.sms_o_amnt'
+            }, 
+            'data_main_vol': {
+                '$sum': '$usage.usage_op.data_main_vol'
+            }, 
+            'data_amnt': {
+                '$sum': '$usage.usage_op.data_amnt'
+            }, 
+            'usage_2G': {
+                '$sum': '$usage.usage_op.usage_2G'
+            }, 
+            'usage_3G': {
+                '$sum': '$usage.usage_op.usage_3G'
+            }, 
+            'usage_4G_TDD': {
+                '$sum': '$usage.usage_op.usage_4G_TDD'
+            }, 
+            'usage_4G_FDD': {
+                '$sum': '$usage.usage_op.usage_4G_FDD'
+            }, 
+            'usage_4G_4G+': {
+                '$sum': '$usage.usage_op.usage_4G_4G+'
+            }, 
+            'data_bndl_vol': {
+                '$sum': '$usage.usage_op.data_bndl_vol'
+            }, 
+            'voice_vas_cnt': {
+                '$sum': '$usage.usage_op.voice_vas_cnt'
+            }, 
+            'voice_vas_amnt': {
+                '$sum': '$usage.usage_op.voice_vas_amnt'
+            }, 
+            'voice_vas_main_vol': {
+                '$sum': '$usage.usage_op.voice_vas_main_vol'
+            }, 
+            'voice_vas_bndl_vol': {
+                '$sum': '$usage.usage_op.voice_vas_bndl_vol'
+            }, 
+            'sms_vas_cnt': {
+                '$sum': '$usage.usage_op.sms_vas_cnt'
+            }, 
+            'sms_vas_bndl_cnt': {
+                '$sum': '$usage.usage_op.sms_vas_bndl_cnt'
+            }, 
+            'sms_vas_amnt': {
+                '$sum': '$usage.usage_op.sms_vas_amnt'
+            }
+        }
+    }
+]
+    db = client['cbm']
+    collection = db['daily_usage']
+    resultat = collection.aggregate(pipeline)
+    print("reussie")
+'''
   
 
 if __name__ == "__main__":
-    client = pymongo.MongoClient("mongodb://oma_dwh:Dwh4%40OrnZ@192.168.61.199:27017/?authMechanism=DEFAULT")
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
     date = sys.argv[1]
-    datetime = datetime.strptime(date,'%Y-%m-%d').date()
+    date_time = datetime.strptime(date,'%Y-%m-%d')
+    #getTotal_usage_jour_daily_usage(client,date_time)
     
