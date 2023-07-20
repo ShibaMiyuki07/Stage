92ea0d81fa8e90e1c8d0dba36d25c8493b28e141
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-07-20 10:12:02 +0300

    Update Verification_Total.py

--- Sprint1/Verification_Total.py
+++ Sprint1/Verification_Total.py
@@ -2,8 +2,103 @@ import pymongo
 import sys
 from datetime import datetime
 
-#def getTotal_usage_jour_global_daily_usage(client,day):
-
+def getTotal_usage_jour_global_daily_usage(client,day):
+    pipeline = [
+    {
+        '$match': {
+            'day': day, 
+            'usage_type': 'usage'
+        }
+    }, {
+        '$group': {
+            '_id': '$day', 
+            'sms_i_cnt': {
+                '$sum': '$sms_i_cnt'
+            }, 
+            'voice_i_cnt': {
+                '$sum': '$voice_i_cnt'
+            }, 
+            'voice_i_vol': {
+                '$sum': '$voice_i_vol'
+            }, 
+            'voice_i_amnt': {
+                '$sum': '$voice_i_amnt'
+            }, 
+            'voice_o_cnt': {
+                '$sum': '$voice_o_cnt'
+            }, 
+            'voice_o_main_vol': {
+                '$sum': '$voice_o_main_vol'
+            }, 
+            'voice_o_amnt': {
+                '$sum': '$voice_o_amnt'
+            }, 
+            'voice_o_bndle_vol': {
+                '$sum': '$voice_o_bndl_vol'
+            }, 
+            'sms_o_main_cnt': {
+                '$sum': '$sms_o_main_cnt'
+            }, 
+            'sms_o_bndl_cnt': {
+                '$sum': '$sms_o_bndl_cnt'
+            }, 
+            'sms_o_amnt': {
+                '$sum': '$sms_o_amnt'
+            }, 
+            'data_main_vol': {
+                '$sum': '$data_main_vol'
+            }, 
+            'data_amnt': {
+                '$sum': '$data_amnt'
+            }, 
+            'usage_2G': {
+                '$sum': '$usage_2G'
+            }, 
+            'usage_3G': {
+                '$sum': '$usage_3G'
+            }, 
+            'usage_4G_TDD': {
+                '$sum': '$usage_4G_TDD'
+            }, 
+            'usage_4G_FDD': {
+                '$sum': '$usage_4G_FDD'
+            }, 
+            'usage_4G_4G+': {
+                '$sum': '$usage_4G_4G+'
+            }, 
+            'data_bndl_vol': {
+                '$sum': '$data_bndl_vol'
+            }, 
+            'voice_vas_cnt': {
+                '$sum': '$voice_vas_cnt'
+            }, 
+            'voice_vas_amnt': {
+                '$sum': '$voice_vas_amnt'
+            }, 
+            'voice_vas_main_vol': {
+                '$sum': '$voice_vas_main_vol'
+            }, 
+            'voice_vas_bndl_vol': {
+                '$sum': '$voice_vas_bndl_vol'
+            }, 
+            'sms_vas_cnt': {
+                '$sum': '$sms_vas_cnt'
+            }, 
+            'sms_vas_bndl_cnt': {
+                '$sum': '$sms_vas_bndl_cnt'
+            }, 
+            'sms_vas_amnt': {
+                '$sum': '$sms_vas_amnt'
+            }
+        }
+    }
+]
+    db = client['cbm']
+    collection = db['daily_usage']
+    resultat = collection.aggregate(pipeline)
+    for r in resultat:
+        print(r)
+    print("reussie")
 
 '''def getTotal_usage_jour_daily_usage(client,day):
     pipeline = [
@@ -118,5 +213,7 @@ if __name__ == "__main__":
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     date = sys.argv[1]
     date_time = datetime.strptime(date,'%Y-%m-%d')
+    day = datetime(date_time.year,date_time.month,date_time.day)
+    print(day)
     #getTotal_usage_jour_daily_usage(client,date_time)
-    
+    getTotal_usage_jour_global_daily_usage(client,day)
