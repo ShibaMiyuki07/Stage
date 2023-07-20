55d5fe2e99c19876d69d4a5683cb64598e672551
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-07-20 16:00:32 +0300

    Update Verification_Total.py

--- Sprint1/Verification_Total.py
+++ Sprint1/Verification_Total.py
@@ -451,7 +451,7 @@ def comparaison_donne(global_daily_usage,daily_usage,day):
   
 
 if __name__ == "__main__":
-    client = pymongo.MongoClient("mongodb://oma_dwh:Dwh4%40OrnZ@192.168.61.199:27017/?authMechanism=DEFAULT")
+    client = pymongo.MongoClient("mongodb://localhost:27017")
     date = sys.argv[1]
     date_time = datetime.strptime(date,'%Y-%m-%d')
     day = datetime(date_time.year,date_time.month,date_time.day)
