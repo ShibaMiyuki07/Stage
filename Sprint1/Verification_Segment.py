e8d5c4139f6a608b9dba4faf2da5b6fee30bdfa4
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-07-25 10:58:42 +0300

    Update Verification_Segment.py

---
+++ Sprint1/Verification_Segment.py
@@ -0,0 +1,230 @@
+from datetime import datetime
+import sys
+import pymongo
+
+from fonction_usage import calcul_error_usage, data_daily_usage, data_usage_global
+
+def getdata_tmp(client,day):
+    pipeline = [
+        {
+            '$match' : {
+                '$day' : day
+            }
+        },
+    {
+        '$group': {
+            '_id': '$segment', 
+            'sms_i_cnt': {
+                '$sum': '$sms_i_cnt'
+            }, 
+            'voice_i_cnt': {
+                '$sum': '$voice_i_cnt'
+            }, 
+            'voice_i_vol': {
+                '$sum': '$voice_i_vol'
+            }, 
+            'voice_i_amnt': {
+                '$sum': '$voice_i_amnt'
+            }, 
+            'voice_o_cnt': {
+                '$sum': '$voice_o_cnt'
+            }, 
+            'voice_o_main_vol': {
+                '$sum': '$voice_o_main_vol'
+            }, 
+            'voice_o_amnt': {
+                '$sum': '$voice_o_amnt'
+            }, 
+            'voice_o_bndle_vol': {
+                '$sum': '$voice_o_bndl_vol'
+            }, 
+            'sms_o_main_cnt': {
+                '$sum': '$sms_o_main_cnt'
+            }, 
+            'sms_o_bndl_cnt': {
+                '$sum': '$sms_o_bndl_cnt'
+            }, 
+            'sms_o_amnt': {
+                '$sum': '$sms_o_amnt'
+            }, 
+            'data_main_vol': {
+                '$sum': '$data_main_vol'
+            }, 
+            'data_amnt': {
+                '$sum': '$data_amnt'
+            }, 
+            'usage_2G': {
+                '$sum': '$usage_2G'
+            }, 
+            'usage_3G': {
+                '$sum': '$usage_3G'
+            }, 
+            'usage_4G_TDD': {
+                '$sum': '$usage_4G_TDD'
+            }, 
+            'usage_4G_FDD': {
+                '$sum': '$usage_4G_FDD'
+            }, 
+            'usage_4G_4G+': {
+                '$sum': '$usage_4G_4G+'
+            }, 
+            'data_bndl_vol': {
+                '$sum': '$data_bndl_vol'
+            }, 
+            'voice_vas_cnt': {
+                '$sum': '$voice_vas_cnt'
+            }, 
+            'voice_vas_amnt': {
+                '$sum': '$voice_vas_amnt'
+            }, 
+            'voice_vas_main_vol': {
+                '$sum': '$voice_vas_main_vol'
+            }, 
+            'voice_vas_bndl_vol': {
+                '$sum': '$voice_vas_bndl_vol'
+            }, 
+            'sms_vas_cnt': {
+                '$sum': '$sms_vas_cnt'
+            }, 
+            'sms_vas_bndl_cnt': {
+                '$sum': '$sms_vas_bndl_cnt'
+            }, 
+            'sms_vas_amnt': {
+                '$sum': '$sms_vas_amnt'
+            }
+        }
+    }
+]
+    db = client['tmp']
+    collection = db['tmp_daily_segment']
+    retour = {}
+    resultat = collection.aggregate(pipeline,cursor={})
+    for r in resultat:
+        if r['_id'] != None:
+            retour[r['_id']] = data_daily_usage(r)
+        else: retour["null"] = data_daily_usage(r)
+    return retour
+
+def getdata_global_daily(client,day):
+    pipeline = [
+    {
+        '$match': {
+            'day': day, 
+            'usage_type': 'usage'
+        }
+    }, {
+        '$group': {
+            '_id': '$segment', 
+            'sms_i_cnt': {
+                '$sum': '$sms_i_cnt'
+            }, 
+            'voice_i_cnt': {
+                '$sum': '$voice_i_cnt'
+            }, 
+            'voice_i_vol': {
+                '$sum': '$voice_i_vol'
+            }, 
+            'voice_i_amnt': {
+                '$sum': '$voice_i_amnt'
+            }, 
+            'voice_o_cnt': {
+                '$sum': '$voice_o_cnt'
+            }, 
+            'voice_o_main_vol': {
+                '$sum': '$voice_o_main_vol'
+            }, 
+            'voice_o_amnt': {
+                '$sum': '$voice_o_amnt'
+            }, 
+            'voice_o_bndle_vol': {
+                '$sum': '$voice_o_bndl_vol'
+            }, 
+            'sms_o_main_cnt': {
+                '$sum': '$sms_o_main_cnt'
+            }, 
+            'sms_o_bndl_cnt': {
+                '$sum': '$sms_o_bndl_cnt'
+            }, 
+            'sms_o_amnt': {
+                '$sum': '$sms_o_amnt'
+            }, 
+            'data_main_vol': {
+                '$sum': '$data_main_vol'
+            }, 
+            'data_amnt': {
+                '$sum': '$data_amnt'
+            }, 
+            'usage_2G': {
+                '$sum': '$usage_2G'
+            }, 
+            'usage_3G': {
+                '$sum': '$usage_3G'
+            }, 
+            'usage_4G_TDD': {
+                '$sum': '$usage_4G_TDD'
+            }, 
+            'usage_4G_FDD': {
+                '$sum': '$usage_4G_FDD'
+            }, 
+            'usage_4G_4G+': {
+                '$sum': '$usage_4G_4G+'
+            }, 
+            'data_bndl_vol': {
+                '$sum': '$data_bndl_vol'
+            }, 
+            'voice_vas_cnt': {
+                '$sum': '$voice_vas_cnt'
+            }, 
+            'voice_vas_amnt': {
+                '$sum': '$voice_vas_amnt'
+            }, 
+            'voice_vas_main_vol': {
+                '$sum': '$voice_vas_main_vol'
+            }, 
+            'voice_vas_bndl_vol': {
+                '$sum': '$voice_vas_bndl_vol'
+            }, 
+            'sms_vas_cnt': {
+                '$sum': '$sms_vas_cnt'
+            }, 
+            'sms_vas_bndl_cnt': {
+                '$sum': '$sms_vas_bndl_cnt'
+            }, 
+            'sms_vas_amnt': {
+                '$sum': '$sms_vas_amnt'
+            }
+        }
+    }
+]
+    db = client['cbm']
+    collection = db['global_daily_usage']
+    retour = {}
+    resultat = collection.aggregate(pipeline,cursor={})
+    for r in resultat:
+        if r['_id'] != None : retour[r['_id']] = data_usage_global(r)
+        else : retour["null"] = data_usage_global(r)
+    return retour
+
+def comparaison_donne(global_daily_usage,daily_usage,liste_segment):
+    for i in range(len(liste_segment)):
+        if liste_segment[i] in daily_usage and liste_segment[i] in global_daily_usage:
+            daily_data = daily_usage[liste_segment[i]]
+            global_data = global_daily_usage[liste_segment[i]]
+            if not calcul_error_usage(global_data,daily_data):
+                print("Erreur de donne dans segment "+liste_segment[i].__str__())
+        elif liste_segment[i] in daily_usage and liste_segment[i] not in global_daily_usage:
+            print("Donne du segment "+liste_segment[i].__str__()+" non existant dans global usage")
+        elif liste_segment[i] not in daily_usage and liste_segment[i] in global_daily_usage:
+            print("Donne du segment "+liste_segment[i].__str__()+" non existant dans daily usage")
+        elif liste_segment[i] not in daily_usage and liste_segment[i] not in global_daily_usage:
+            pass
+    
+if __name__ == "__main__":
+    client = pymongo.MongoClient("")
+    liste_segment = ["ZERO","SUPER LOW VALUE","LOW VALUE","MEDIUM","HIGH","SUPER HIGH VALUE","NEW","RETURN","VALUE","null"]
+    date = sys.argv[1]
+    date_time = datetime.strptime(date,'%Y-%m-%d')
+    day = datetime(date_time.year,date_time.month,date_time.day)
+    global_daily_usage = getdata_global_daily(client,day)
+    daily_usage = getdata_tmp(client,day)
+    comparaison_donne(global_daily_usage,daily_usage,liste_segment)
