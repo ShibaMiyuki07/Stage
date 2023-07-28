caa12cf4da834d9cc241659f21177cc44ef004c4
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-07-28 15:52:21 +0300

    depart
    Verification des donne en un jour

--- Sprint2/Verification_Totale.py
+++ Sprint2/Verification_Totale.py
@@ -1,2 +1,87 @@
-if __name__ == "__main__":
+from datetime import datetime
+import sys
+import pymongo
+from Fonction_usage import calcul_error, insertion_data
+
+
+def getglobal_usage(client,day):
+    pipeline = [
+    {
+        '$match': {
+            'day': day, 
+            'usage_type': 'bundle'
+        }
+    }, {
+        '$group': {
+            '_id': '$day', 
+            'bndle_cnt': {
+                '$sum': '$bndle_cnt'
+            }, 
+            'bundle_amnt': {
+                '$sum': '$bndle_amnt'
+            }
+        }
+    }
+]
+    db = client['cbm']
+    collection = db['global_daily_usage']
+    retour = {}
+    resultat = collection.aggregate(pipeline,cursor={})
+    for r in resultat:
+        retour[day] = insertion_data(r)
+    return retour
+
+
+def getdaily_usage(client,day):
+    pipeline = [
+    {
+        '$match': {
+            'day': day
+        }
+    }, {
+        '$unwind': {
+            'path': '$bundle', 
+            'includeArrayIndex': 'b', 
+            'preserveNullAndEmptyArrays': False
+        }
+    }, {
+        '$unwind': {
+            'path': '$bundle.subscription', 
+            'includeArrayIndex': 'b_s', 
+            'preserveNullAndEmptyArrays': False
+        }
+    }, {
+        '$group': {
+            '_id': '$day', 
+            'bndle_cnt': {
+                '$sum': '$bundle.subscription.bndle_cnt'
+            }, 
+            'bundle_amnt': {
+                '$sum': '$bundle.subscription.bndle_amnt'
+            }
+        }
+    }
+]
+    db = client['cbm']
+    collection = db['daily_usage']
+    retour = {}
+    resultat = collection.aggregate(pipeline,cursor={})
+    for r in resultat:
+        retour[day] = insertion_data(r)
+
+def comparaison_donne(global_daily_usage,daily_usage,day):
+    global_data = global_daily_usage[day]
+    daily_data = daily_usage[day]
+
+    calcul_error(global_data,daily_data)
+
     
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient("mongodb://oma_dwh:Dwh4%40OrnZ@192.168.61.199:27017/?authMechanism=DEFAULT")
+    date = sys.argv[1]
+    date_time = datetime.strptime(date,'%Y-%m-%d')
+    day = datetime(date_time.year,date_time.month,date_time.day)
+    global_daily_usage = getglobal_usage(client,day)
+    daily_usage = getdaily_usage(client,day)
+    comparaison_donne(global_daily_usage,daily_usage,day)
