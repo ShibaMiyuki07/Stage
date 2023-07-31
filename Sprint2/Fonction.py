510a9ecd3077f9455f4eaf1a6ba15502a2716b26
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-07-31 10:27:05 +0300

    maj

--- Sprint2/Fonction.py
+++ Sprint2/Fonction.py
@@ -1,5 +1,16 @@
-from Verification_Bundle import getsubs
+import codecs
+import mysql.connector
 
+def getsubs():
+    connexion = mysql.connector.connect(user='ETL_USER',password='3tl_4ser',host='192.168.61.196',database='DM_RF')
+    cursor = connexion.cursor() 
+    query = "select name from rf_subscriptions where name is not null"
+    cursor.execute(query)
+    subs_list = []
+    for(name) in cursor:
+        subs_list.append(codecs.encode(name[0],"UTF-8"))
+    subs_list.append("null")
+    return subs_list
 
 def insertion_data(r):
     retour = {}
