cbb5fc842c348181d2a2aee4a0d9cd6f30176c13
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-08-02 15:23:45 +0300

    refactorisation verification bundle

---
+++ Sprint2Refactorise/Execution_Requete.py
@@ -0,0 +1,58 @@
+from Utils import getcollection_daily_aggrege, getcollection_global, insertion_data
+
+
+def getdata_daily(day,nom):
+    pipeline =[
+        {
+            '$match': {
+                'day': day,
+                'usage_type': 'topup',
+                'type_aggregation' : nom
+            }
+        },
+        {
+            '$project' : {
+                '_id' : '$'+nom,
+                'rec_cnt' : 1,
+                'rec_amnt' : 1
+            }
+        }
+    ]
+    collection = getcollection_daily_aggrege()
+    resultat = collection.aggregate(pipeline)
+    retour = {}
+    for r in resultat:
+        if r['_id'] != None:
+            retour[r['_id']] = insertion_data(r)
+        else:
+            retour['null'] = insertion_data(r)
+    return retour
+
+def  getglobal_usage(day,nom):
+    pipeline = [
+    {
+        '$match': {
+            'day': day, 
+            'usage_type': 'topup'
+        }
+    }, {
+        '$group': {
+            '_id': '$'+nom, 
+            'rec_cnt': {
+                '$sum': '$rec_cnt'
+            }, 
+            'rec_amnt': {
+                '$sum': '$rec_amnt'
+            }
+        }
+    }
+]
+    collection = getcollection_global()
+    resultat = collection.aggregate(pipeline)
+    retour = {}
+    for r in resultat:
+        if r['_id'] != None:
+            retour[r['_id']] = insertion_data(r)
+        else:
+            retour['null'] = insertion_data(r)
+    return retour
