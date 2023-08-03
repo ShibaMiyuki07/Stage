26ea7b2d7a6c1c6d05b6fee5aab82233c996f325
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-08-03 10:51:03 +0300

    Create Insertion_Data.py

---
+++ Sprint4/Insertion_OM/Insertion_Data.py
@@ -0,0 +1,13 @@
+import mysql.connector
+def getall_site():
+    connexion = mysql.connector.connect(user='ETL_USER',password='3tl_4ser',host='192.168.61.196',database='DM_RF')
+    cursor = connexion.cursor() 
+    query = "select sig_id_site as site_id,sig_nom_site site_name,sig_code_site as site_code from rf_sig_cell_krill_v3 where sig_nom_site is not null"
+    cursor.execute(query)
+    all_site = {}
+    for(site_id,site_name,site_code) in cursor:
+        all_site[site_id[0]] = {'site_name' : site_name[0],'site_code' : site_code[0]}
+    return all_site
+
+if __name__ == "__main__":
+    print(getall_site())
