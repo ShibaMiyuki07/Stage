09c9226113f1e5e4c69bdc04da3ab08dd6049e14
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-08-03 11:02:38 +0300

    Update Insertion_Data.py

--- Sprint4/Insertion_OM/Insertion_Data.py
+++ Sprint4/Insertion_OM/Insertion_Data.py
@@ -1,3 +1,5 @@
+import datetime
+import sys
 import mysql.connector
 def getall_site():
     connexion = mysql.connector.connect(user='ETL_USER',password='3tl_4ser',host='192.168.61.196',database='DM_RF')
@@ -9,5 +11,19 @@ def getall_site():
           all_site[site_id] = {'site_name' : site_name,'site_code' : site_code}
     return all_site
 
+def getmsisdn_location(day):
+    connexion = mysql.connector.connect(user='ETL_USER',password='3tl_4ser',host='192.168.61.196',database='DM_OD')
+    cursor = connexion.cursor() 
+    query = "select msisdn as numero,site_id  from caller_daily_location where upd_dt="+day.__str__()
+    cursor.execute(query)
+    all_msisdn_location = {}
+    for(numero,site_id) in cursor:
+          all_msisdn_location[numero] = {'site_id' : site_id,'numero' : numero}
+    return all_msisdn_location
+
 if __name__ == "__main__":
-    print(getall_site())
+    date = sys.argv[1]
+    date_time = datetime.strptime(date,'%Y-%m-%d')
+    day = datetime(date_time.year,date_time.month,date_time.day)
+    liste_site = getall_site()
+    print(getmsisdn_location(date))
