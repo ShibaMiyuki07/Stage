886a09a50b505176783a2b19e0c11b6d22f044a6
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-08-09 15:02:43 +0300

    Mise a jour Erc

--- Insertion_Data/Insertion_Daily_Erc.py
+++ Insertion_Data/Insertion_Daily_Erc.py
@@ -2,34 +2,15 @@ from datetime import datetime
 import sys
 from Utils import connexion_sql, getcollection_insertion, insertion_data
 
-def getnb_semaine_annee(semaine):
-    query = "select tra_date,sum(nb_msisdn) nb from WORK.final_capillarite_erec_semaine where semaine = %s group by semaine"
-    db = connexion_sql()
-    cursor = db.cursor()
-    value= (semaine)
-    cursor.execute(query,value)
-    for (tra_date,nb) in cursor:
-        return nb
-    
-def getnb_semaine_annee_site(semaine,location):
-    query = "select tra_date,sum(nb_msisdn) nb from WORK.final_capillarite_erec_semaine where semaine = %s and sig_nom_site = %s"
-    db = connexion_sql()
-    cursor = db.cursor()
-    value= (semaine,location)
-    cursor.execute(query,value)
-    for (tra_date,nb) in cursor:
-        return nb
-
 def insertion_day(day):
-    query = "select tra_date,sum(capillarite_erecharge) nb from WORK.final_capillarite_erec_jour where tra_date = %s group by tra_date"
+    query = "select count(distinct(tra_sndr_msisdn)) nb from DWH.zebra_rp2p_transaction where tra_date = %s and (tra_sndr_category = 'RETSA' or tra_sndr_category='RETMGR') and tra_transfer_status = 200 and tra_channel = 'C2S'"
     db = connexion_sql()
     cursor = db.cursor()
     value = (day)
     cursor.execute(query,value)
     data = []
-    for (tra_date,nb) in cursor:
-        semaine = "S"+day.strftime("%V")+'-'+day.year.__str__()
-        data.append({ 'day' : day,'usage_type' : 'e-rc','type_aggregation' : 'day','nb' : nb,'nb_semaine' : getnb_semaine_annee(semaine)})
+    for (nb) in cursor:
+        data.append({ 'day' : day,'usage_type' : 'e-rc','type_aggregation' : 'day','nb' : nb[0]})
     
     insertion_data(getcollection_insertion('tmp_daily_aggregation'),data)
 
@@ -41,8 +22,7 @@ def insertion_site_name(day):
     cursor.execute(query,value)
     data = []
     for (nb,site_name) in cursor:
-        semaine = "S"+day.strftime("%V")+'-'+day.year.__str__()
-        data.append({ 'day' : day,'usage_type' : 'e-rc','type_aggregation' : 'site_name','site_name' : site_name,'nb' : nb,'nb_semaine' : getnb_semaine_annee_site(semaine)})
+        data.append({ 'day' : day,'usage_type' : 'e-rc','type_aggregation' : 'site_name','site_name' : site_name,'nb' : nb})
     
     insertion_data(getcollection_insertion('tmp_daily_aggregation'),data)
 
