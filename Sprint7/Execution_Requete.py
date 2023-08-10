bfc77b3a33a96992e41712eaf5b033faf4ee13cf
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-08-10 14:57:28 +0300

    verification e-rc

---
+++ Sprint7/Execution_Requete.py
@@ -0,0 +1,54 @@
+from Utils import getcollection_daily_aggrege, getcollection_global, insertion_data
+
+
+def getdata_daily(day,nom):
+    pipeline =[
+        {
+            '$match': {
+                'day': day,
+                'usage_type': 'e-rc',
+                'type_aggregation' : nom
+            }
+        },
+        {
+            '$project' : {
+                '_id' : '$'+nom,
+                'nb' : 1,
+            }
+        }
+    ]
+    collection = getcollection_daily_aggrege()
+    resultat = collection.aggregate(pipeline)
+    retour = {}
+    for r in resultat:
+        if r['_id'] != None:
+            retour[r['_id']] = insertion_data(r)
+        else:
+            retour['null'] = insertion_data(r)
+    return retour
+
+def  getglobal_usage(day,nom):
+    pipeline = [
+    {
+        '$match': {
+            'day': day, 
+            'usage_type': 'e-rc'
+        }
+    }, {
+        '$group': {
+            '_id': '$'+nom, 
+             'nb': {
+                '$sum': '$nb'
+            },
+        }
+    }
+]
+    collection = getcollection_global()
+    resultat = collection.aggregate(pipeline)
+    retour = {}
+    for r in resultat:
+        if r['_id'] != None:
+            retour[r['_id']] = insertion_data(r)
+        else:
+            retour['null'] = insertion_data(r)
+    return retour
