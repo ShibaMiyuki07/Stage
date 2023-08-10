c3fa930820f7733a60d3ab3a8390c8cd28b9b6d4
Miyuki <shibamiyuki07@yahoo.com>
2023-08-10 16:47:44 +0300

    Update Insertion_Daily_Erc.py

--- Insertion_Data/Insertion_Daily_Erc.py
+++ Insertion_Data/Insertion_Daily_Erc.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import sys
 from Utils import connexion_sql, getcollection_insertion, insertion_data
 
-def insertion_day(day):
+def insertion_day(day,date):
     query = "select count(distinct(tra_sndr_msisdn)) nb from DWH.zebra_rp2p_transaction where tra_date = '"+day+"' and (tra_sndr_category = 'RETSA' or tra_sndr_category='RETMGR') and tra_transfer_status = 200 and tra_channel = 'C2S'"
     db = connexion_sql()
     cursor = db.cursor()
@@ -10,18 +10,18 @@ def insertion_day(day):
     cursor.execute(query)
     data = []
     for (nb) in cursor:
-        data.append({ 'day' : day,'usage_type' : 'e-rc','type_aggregation' : 'day','nb' : nb[0]})
+        data.append({ 'day' : date,'usage_type' : 'e-rc','type_aggregation' : 'day','nb' : nb[0]})
     
     insertion_data(getcollection_insertion('tmp_daily_aggregation'),data)
 
-def insertion_site_name(day):
-    query = "select sig_nom_site site_name,count(sig_nom_site) nb from WORK.caller_daily_location_ofl ms LEFT JOIN DM_RF.rf_sig_cell_krill_new rf on ms.site_id = rf.sig_id where upd_dt = '"+day+"' and msisdn IN (select distinct(concat('261',substr(tra_sndr_msisdn,2,9))) from DWH.zebra_rp2p_transaction where tra_date='"+day+"' and tra_channel='C2S' and tra_transfer_status=200 and tra_sndr_category in ('RETSA','RETMGR')) group by sig_nom_site"
+def insertion_site_name(day,date):
+    query = "select sig_nom_site site_name,count(sig_nom_site) as nb from WORK.caller_daily_location_ofl ms LEFT JOIN DM_RF.rf_sig_cell_krill_new rf on ms.site_id = rf.sig_id where upd_dt = '"+day+"' and msisdn IN (select distinct(concat('261',substr(tra_sndr_msisdn,2,9))) from DWH.zebra_rp2p_transaction where tra_date='"+day+"' and tra_channel='C2S' and tra_transfer_status=200 and tra_sndr_category in ('RETSA','RETMGR')) group by sig_nom_site"
     db = connexion_sql()
     cursor = db.cursor()
     cursor.execute(query)
     data = []
     for (site_name,nb) in cursor:
-        data.append({ 'day' : day,'usage_type' : 'e-rc','type_aggregation' : 'site_name','site_name' : site_name,'nb' : nb})
+        data.append({ 'day' : date,'usage_type' : 'e-rc','type_aggregation' : 'site_name','site_name' : site_name,'nb' : nb})
     
     insertion_data(getcollection_insertion('tmp_daily_aggregation'),data)
 
@@ -30,6 +30,6 @@ if __name__ == "__main__":
     date_time = datetime.strptime(date,'%Y-%m-%d')
     day = datetime(date_time.year,date_time.month,date_time.day)
     date_normal = day.year.__str__() + "-"+day.month.__str__()+"-"+day.day.__str__()
-    insertion_day(date_normal)
-    insertion_site_name(date_normal)
+    insertion_day(date_normal,day)
+    insertion_site_name(date_normal,day)
     
