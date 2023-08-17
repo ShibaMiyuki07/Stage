0afa0e7ffcebcd53e0f7aa1625f32389b33f95ff
Manitra <shibamiyuki07@yahoo.com>
2023-08-17 14:38:43 +0300

    change link

--- angular/dashboard/src/app/api.service.ts
+++ angular/dashboard/src/app/api.service.ts
@@ -6,48 +6,51 @@ import { HttpClient } from "@angular/common/http";
     providedIn : "root"
 })
 
+
+
 export class ApiService{
+    link = "http://127.0.0.1:8000";
     constructor(private HttpClient : HttpClient){}
 
 
     public getBundle(page : number)
     {
-        return this.HttpClient.get('http://127.0.0.1:8000/bundle/'+page)
+        return this.HttpClient.get(this.link+'/bundle/'+page)
     }
 
     public getTopup(page : number)
     {
-        return this.HttpClient.get('http://127.0.0.1:8000/topup/'+page)
+        return this.HttpClient.get(this.link+'/topup/'+page)
     }
 
     public getOm(page : number)
     {
-        return this.HttpClient.get('http://127.0.0.1:8000/om/'+page)
+        return this.HttpClient.get(this.link+'/om/'+page)
     }
 
     public getUsage(page : number)
     {
-        return this.HttpClient.get('http://127.0.0.1:8000/usage/'+page)
+        return this.HttpClient.get(this.link+'/usage/'+page)
     }
 
     public getEc(page : number)
     {
-        return this.HttpClient.get('http://127.0.0.1:8000/ec/'+page)
+        return this.HttpClient.get(this.link+'/ec/'+page)
     }
 
     public getE_rc(page : number)
     {
-        return this.HttpClient.get('http://127.0.0.1:8000/e-rc/'+page)
+        return this.HttpClient.get(this.link+'/e-rc/'+page)
     }
 
     public getRoaming(page : number)
     {
-        return this.HttpClient.get('http://127.0.0.1:8000/roaming/'+page)
+        return this.HttpClient.get(this.link+'/roaming/'+page)
     }
 
     public getdetails(day :string,type : Int16Array)
     {
-        return this.HttpClient.get('http://127.0.0.1:8000/details/'+day+'/'+type)
+        return this.HttpClient.get(this.link+'/details/'+day+'/'+type)
     }
 }
 
