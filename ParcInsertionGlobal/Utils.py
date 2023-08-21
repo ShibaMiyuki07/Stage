6bf915b55d8359e50a4094b24ea413936f78054c
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-08-21 10:16:13 +0300

    insertion details parc

---
+++ ParcInsertionGlobal/Utils.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+import pymongo
+import mysql.connector
+
+
+def connexion_base():
+    client = pymongo.MongoClient("mongodb://oma_dwh:Dwh4%40OrnZ@192.168.61.199:27017/?authMechanism=DEFAULT")
+    return client
+
+def getcollection_daily_usage():
+    client = connexion_base()
+    db = client['cbm']
+    collection = db['daily_usage']
+    return collection
+
+def getcollection_segment():
+    client = connexion_base()
+    db = client['cbm']
+    collection = db['segment']
+    return collection
+
+def connexion_sql():
+    connexion = mysql.connector.connect(user='ETL_USER',password='3tl_4ser',host='192.168.61.196')
+    return connexion
+
+def getall_site():
+    cursor = connexion_sql().cursor() 
+    query = "select max(sig_code_site) site,sig_nom_site nom_site,sig_secteur_name_v3 secteur from DM_RF.rf_sig_cell_krill_v3 sig group by sig_nom_site"
+    cursor.execute(query)
+    all_site = {}
+    for(site,nom_site,secteur) in cursor:
+        all_site[nom_site] = {}
+        all_site[nom_site]['secteur'] = secteur
+        all_site[nom_site]['code'] = site
+    return all_site
+
+def getlastmonth(day):
+    last_month = ""
+    if day.month == 1:
+        date_to_use = datetime(day.year-1,12,1)
+        last_month = date_to_use.year.__str__()+date_to_use.month.__str__()
+    else:
+        date_to_use = datetime(day.year,day.month-1,1)
+        if date_to_use.month <10:
+            last_month = date_to_use.year.__str__()+"0"+date_to_use.month.__str__()
+        else:
+            last_month = date_to_use.year.__str__()+"0"+date_to_use.month.__str__()
+    return last_month
+
+
+def getsegment(day):
+    last_month = getlastmonth(day)
+    collection = getcollection_daily_usage()
+    resultats = collection.find({'day' : last_month})
+    retour = {}
+    for r in resultats : 
+        retour[r['party_id']] = r['vbs_Segment_month']
+    return retour
+
+def calcul_date(debut,fin):
+    difference = debut - fin
+    return difference
+
+def getcollection_to_insert_parc():
+    client = connexion_base()
+    db = client['test']
+    collection = db['parc_details']
+    return collection
---
+++ ParcInsertionGlobal/main.py
@@ -0,0 +1,73 @@
+from datetime import datetime, timedelta
+import sys
+from Utils import calcul_date, getall_site, getcollection_daily_usage, getcollection_to_insert_parc, getsegment
+
+def insertion_data(day,site,segment):
+    collection = getcollection_daily_usage()
+    resultats = collection.find({'day' : day})
+    data = []
+    for r in resultats:
+        insertion = {}
+        insertion['day'] = day
+        insertion['billing_type'] = r['billing_type']
+        insertion['market'] = r['market_id']
+        insertion['gender'] = r['gender']
+        insertion['segment'] = segment[r['party_id']]
+        insertion['site_name'] = r['last_location_name']
+        insertion['site_code'] = site[r['last_location_name']]['code']
+        insertion['secteur'] = site[r['last_location_name']]['secteur']
+        difference_anciennete = calcul_date(day,r['first_event_date']).days
+        anciennete = ""
+        if difference_anciennete<90:
+            anciennete = "[0-3 mois]"
+        elif 90<difference_anciennete and difference_anciennete<=180:
+            anciennete = "]3-6 mois]"
+        elif difference_anciennete<180 and difference_anciennete<=365:
+            anciennete = "]6-12 mois]"
+        elif difference_anciennete>365:
+            anciennete = "]+12 mois]"
+        insertion['anciennete'] = anciennete
+
+        age = ""
+        age_actuelle = (r['birthday_date'] - day).year
+        if 18<age_actuelle and age_actuelle<=25:
+            age = "Fresh"
+        elif 25<age_actuelle and age_actuelle<=45:
+            age = "Master"
+        elif 45<age_actuelle and age_actuelle<=60:
+            age = "Senior"
+        elif 60<age_actuelle:
+            age = "Veteran"
+
+        insertion['age'] = age 
+
+        if r['orange_base_status'] == 'active' or r['orange_base_status'] == 'reactivated' or r['orange_base_status'] == 'new':
+            insertion['parc_FT'] = 1
+        if day == datetime(r['first_event_date'].year,r['first_event_date'].month,r['first_event_date'].day):
+            insertion['activation'] = 1
+        if r['orange_base_status'] == 'reactivated':
+            insertion['reconnexion'] =1
+        if r['orange_base_status'] == 'churn':
+            insertion['deconnexion'] = 1
+        if day == datetime(r['last_topup_date'].year,r['last_topup_date'].month,r['last_topup_date'].day):
+            insertion['parc_recharge_1j'] = 1
+        if calcul_date(day,r['last_topup_date']<30):
+            insertion['parc_recharge_30j'] = 1
+
+        day_29_j = day - timedelta(29)
+        if (r['last_event_o_date'] <day and r['last_event_o_date']>day_29_j) or (r['last_data_date'] <day and r['last_data_date'] > day_29_j) or (r['last_om_transaction_date']<day and r['last_om_transaction_date']>day_29_j):
+            insertion['charged_base'] = 1
+
+        data.append(insertion)
+        
+    getcollection_to_insert_parc().insert_many(data)
+
+
+
+if __name__ == "__main__":
+    date = sys.argv[1]
+    date_time = datetime.strptime(date,'%Y-%m-%d')
+    day = datetime(date_time.year,date_time.month,date_time.day)
+    liste_site = getall_site()
+    liste_segment = getsegment()
+    insertion_data(day,liste_site,liste_segment)
