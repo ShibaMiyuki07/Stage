403a912f9c2b565bfc3d56ff40adcd2aecea4629
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-08-25 09:54:56 +0300

    ajtou retraitement et generalisation de la liste

--- api_python/main.py
+++ api_python/main.py
@@ -4,6 +4,7 @@ from Utils import getusage_type
 from database.Connexion import get_aggregation, getverification_collection
 from Model.Verification import Verification
 import uvicorn
+import os
 
 origins = ['*']
 
@@ -16,48 +17,13 @@ app.add_middleware(CORSMiddleware,
                    allow_headers = ['*'])
 
 
-@app.get('/bundle/{page}')
-async def verification_bundle(page : int):
+@app.get('/liste/{type}/{page}')
+async def liste(type : int,page : int):
     collection = getverification_collection()
-    resultat = collection.find({"usage_type" : 'bundle'}).skip(page*5).limit(5).sort('day',-1)
-    return [Verification.insertion_data(r) for r in resultat]
-
-
-@app.get('/topup/{page}')
-async def verification_topup(page : int):
-    collection = getverification_collection()
-    resultat = collection.find({"usage_type" : 'topup'}).skip(page*5).limit(5).sort('day',-1)
-    return [Verification.insertion_data(r) for r in resultat]
-
-@app.get('/om/{page}')
-async def verification_om(page : int):
-    collection = getverification_collection()
-    resultat = collection.find({"usage_type" : 'om'}).skip(page*5).limit(5).sort('day',-1)
-    return [Verification.insertion_data(r) for r in resultat]
-
-@app.get('/usage/{page}')
-async def verification_usage(page : int):
-    collection = getverification_collection()
-    resultat = collection.find({"usage_type" : 'usage'}).skip(page*5).limit(5).sort('day',-1)
-    return [Verification.insertion_data(r) for r in resultat]
-
-@app.get('/ec/{page}')
-async def verification_ec(page : int):
-    collection = getverification_collection()
-    resultat = collection.find({"usage_type" : 'ec'}).skip(page*5).limit(5).sort('day',-1)
-    return [Verification.insertion_data(r) for r in resultat]
-
-@app.get('/e-rc/{page}')
-async def verification_e_rc(page : int):
-    collection = getverification_collection()
-    resultat = collection.find({"usage_type" : 'e-rc'}).skip(page*5).limit(5).sort('day',-1)
-    return [Verification.insertion_data(r) for r in resultat]
-
-@app.get('/roaming/{page}')
-async def verification_roaming(page : int):
-    collection = getverification_collection()
-    resultat = collection.find({"usage_type" : 'roaming'}).skip(page*5).limit(5).sort('day',-1)
-    return [Verification.insertion_data(r) for r in resultat]
+    usage_type = getusage_type(type)
+    nbr_doc = collection.count_documents({"usage_type" : usage_type})
+    resultat = collection.find({"usage_type" : usage_type}).skip((page-1)*5).limit(5).sort('day',-1)
+    return {'usage_type' : usage_type,'nbr_doc' : nbr_doc,'data' : [Verification.insertion_data(r) for r in resultat]}
 
 
 @app.get('/details/{date}/{type}')
@@ -84,6 +50,28 @@ async def dashboard_bundle(type : int):
     usage_type = getusage_type(type)
     resultats = collection.find({'usage_type' : usage_type,'type_aggregation' : 'day'}).sort('day',-1).limit(8).sort('day',1)
     return {'data' : [Verification.insertion_data(r) for r in resultats]}
+
+
+@app.get('/retraitement/{date}/{type}')
+async def retraitement(date : str,type : int):
+    day = Verification.remplacement_date(date)
+    collection = get_aggregation()
+    usage_type = getusage_type(type)
+    count = 0
+    resultats = collection.find({'day' : day,'usage_type' : usage_type,'type_aggregation' : 'day'})
+    for r in resultats:
+        count +=1
+    log = "Impossible de lancer le retraitement car les données n'existe pas"
+    '''if count != 0:
+        cmd = ""
+        os.system(cmd)
+        directory = ""
+        fichier_a_lancer = directory+"/"+usage_type+"/main.py"
+        cmd_reverification = "python "+fichier_a_lancer+" "+date
+        os.system(cmd_reverification)'''
+    return log
+
+
 
 if __name__ == "__main__":
      uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
