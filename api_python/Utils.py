2886d4fee5c4a5c666cc3407c5150757d9bf9d92
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-09-29 08:52:20 +0300

    Update Utils.py
    update om

--- api_python/Utils.py
+++ api_python/Utils.py
@@ -58,11 +58,16 @@ def verification_donne(day_debut,day_fin,usage_type,en_cours):
                 break
             day_actuelle = day_actuelle + timedelta(1)
         else:   
-            directory_insertion="/data2/tmp/Stage/dossier_final/om/Insertion_data/Extraction_Data.py"
+            directory_extraction="/data2/tmp/Stage/dossier_final/om/Insertion_data/Extraction_Data.py"
             date = day_actuelle.strftime("%Y-%m-%d")
-            cmd_insertion = 'plink -ssh osadmin@192.168.61.111 -pw osadmin@321 "source /data/temp/venv_test/bin/activate ; python -u '+directory_insertion+' '+date+'; deactivate"'
-            print(cmd_insertion)
-            subprocess.run(cmd_insertion,shell=True)
+            cmd_insertion = 'plink -ssh osadmin@192.168.61.111 -pw osadmin@321 "source /data/temp/venv_test/bin/activate ; python -u [directory] '+date+'; deactivate"'
+            to_extract = cmd_insertion.replace('[directory]',directory_extraction)
+            subprocess.run(to_extract,shell=True)
+
+            directory_extraction="/data2/tmp/Stage/dossier_final/om/Insertion_data/Insertion_Data.py"
+            to_insert = cmd_insertion.replace('[directory]',directory_extraction)
+            subprocess.run(to_insert,shell=True)
+
 
             directory_verification="/data2/tmp/Stage/dossier_final/"+usage_type+"/main.py"
             cmd_verification='plink -ssh osadmin@192.168.61.111 -pw osadmin@321 "source /data/temp/venv_test/bin/activate ;python -u '+directory_verification+' '+date+';deactivate"'
